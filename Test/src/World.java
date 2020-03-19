3408f06488758cc1ff68a3437ad1377c5f9d4921
Pavel Litvinenka <pasblek@yandex.ru>
2020-03-19 14:11:52 +0300

    Ver

---
+++ Test/src/World.java
@@ -0,0 +1,12 @@
+
+public class World {
+
+	public static void main(String[] args) {
+		// TODO Автоматически созданная заглушка метода
+		System.out.println("Привет Мир !!, Я познаю Git");
+		System.out.println("Привет Мир !!, Я познаю Git");
+		
+
+	}
+
+}
