5e5c44edc2016748fbf0e2f0efe0dc90a3314943
Pavel Litvinenka <pasblek@yandex.ru>
2020-03-19 14:24:00 +0300

    ver 3

--- Test/src/World.java
+++ Test/src/World.java
@@ -6,6 +6,7 @@ public class World {
 		System.out.println("Привет Мир !!, Я познаю Git");
 		System.out.println("Привет Мир !!, Я познаю Git");
 		System.out.println("У меня наконец-то получилось добавить в Git Hub свой проект :)");
+		System.out.println("я смог обновить репозиторий");
 		
 
 	}
